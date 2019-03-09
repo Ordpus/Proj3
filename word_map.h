495a7710a8491c593eef2a8b1d2a4a22a306fbcf
Ordpus <12138313+Ordpus@users.noreply.github.com>
2019-03-09 15:56:03 -0500

    Update word_map.h

--- word_map.h
+++ word_map.h
@@ -4,12 +4,12 @@
 typedef struct entry {
   char key[32];
   int value;
-  int hash;
+  unsigned int hash;
   struct entry* next;
 } Entry;
 
-int hash(Entry* entry);
-int hash_key(char* str);
+unsigned int hash(Entry* entry);
+unsigned int hash_key(char* str);
 int putVal(Entry** map, Entry* entry, int map_length);
 Entry* getVal(Entry** map, char* key, int map_length);
 void printMap(Entry** map, int map_length);
